f243e8d6cb7a3c3eeb9bd306a2a298bd2ea95640
RyanSieiro-Dev <sieiroryan@outlook.com>
2024-08-29 19:07:07 -0300

    Desafio concluído

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -15,48 +15,84 @@ namespace DesafioFundamentos.Models
         public void AdicionarVeiculo()
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+            
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+
+            string placa = Console.ReadLine();
+
+            if (placa.Length == 8 && placa.Contains("-")) // Verificar se a placa tem um formato específico
+        {
+            veiculos.Add(placa);
+            
+            Console.WriteLine($"Veículo com placa: {placa} adicionado com sucesso!");
+        }
+        else {
+            Console.WriteLine("placa inválida, tente novamente.");
+
+        }
         }
 
+        
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
+                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal   
 
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            
             // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            
+            string placa = Console.ReadLine();
+           
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+            Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+            string horasString = Console.ReadLine();
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+            if (int.TryParse(horasString, out int horas))
+            {
+               
+            decimal valorTotal = precoInicial + precoPorHora * horas; 
+            
+            
+            // TODO: Remover a placa digitada da lista de veículos
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+            Console.WriteLine($"Você deseja remover o veículo {placa}? (S/N)");
+            string resposta = Console.ReadLine();
 
+            if (resposta == "S")
+            {
+                veiculos.Remove(placa);
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+            }
+
+            else
+            {
+                Console.WriteLine("Operação cancelada.");
+            }
+                // TODO: Remover a placa digitada da lista de veículos
+               
             }
             else
             {
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
             }
         }
-
+        }
         public void ListarVeiculos()
         {
             // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
+
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+            
+
+                for(int cont = 0; cont < veiculos.Count; cont++){
+                Console.WriteLine($"Estacionamento Nº {cont + 1} - Placa: {veiculos[cont]}");
+            }
             }
             else
             {
